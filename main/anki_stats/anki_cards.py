4eb77d5962c8d33e026dd8eb23cf69dcf7204271
Tyler Wafkowski <tylerwaf@gmail.com>
2023-12-19 08:53:58 -0500

    Improving speed of stats script

--- main/anki_stats/anki_cards.py
+++ main/anki_stats/anki_cards.py
@@ -6,46 +6,16 @@ class AnkiCards:
 
   def __get_cards(self) -> type:
     return invoke_anki_request('findCards', query=f'deck:{self.deck_name}')
+  
+  def get_deck_name(self) -> str:
+    return self.deck_name.replace("_", " ")
 
-  def __get_card_statistics(self) -> type:
+  def get_card_statistics(self) -> type:
     anki_cards = self.__get_cards()
     return invoke_anki_request('cardsInfo', cards=anki_cards)
-
-  def card_interval_stats(self) -> dict:
-    card_stats = self.__get_card_statistics()
-
-    mature_cards = []
-    young_cards = []
-
-    for card in card_stats:
-      card_interval = card['interval']
-
-      if int(card_interval) >= 21:
-        mature_cards.append(card_interval)
-      elif int(card_interval) < 21 and int(card_interval) != 0:
-        young_cards.append(card_interval)
-
-    total_cards_known = int(len(mature_cards)) + int(len(young_cards))
-
-    return { 'young_cards': int(len(young_cards)), 'mature_cards': int(len(mature_cards)), 'total_cards_known': total_cards_known}
-    
-  def get_new_cards(self) -> int:
-    card_stats = self.__get_card_statistics()
-
-    new_cards = []
-
-    for card in card_stats:
-      card_interval = card['interval']
-
-      if int(card_interval) == 0:
-        new_cards.append(card_interval)
-
-    return int(len(new_cards))
   
   def get_new_cards_per_day_amount(self) -> type:
     deck_name = self.deck_name.replace("_", " ")
     deck_info = invoke_anki_request('getDeckConfig', deck=deck_name)
 
     return deck_info['new']['perDay']
-
-
